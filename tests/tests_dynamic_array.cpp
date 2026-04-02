aa5b204da71033b7c7d620d203e705bc66cf28ad
daria <bdrdria@yandex.ru>
2026-04-02 23:38:43 +0500

    docs: add docs for dynamic array class

--- tests/tests_dynamic_array.cpp
+++ tests/tests_dynamic_array.cpp
@@ -1,11 +1,21 @@
 #include <stdexcept>
 #include <new>
 
+/**
+ * @brief Шаблонный класс динамического массива
+ * 
+ * @tparam T тип элементов
+ */
 template <typename T> class DynamicArray {
     private:
         T* data;
         size_t size;
         size_t capacity;
+        /**
+         * @brief Выделяет память для динамического массива без инициализации
+         * 
+         * @param cap вместимость массива
+         */
         void memory_allocate(size_t cap) {
             if (data != nullptr) {
                 throw std::logic_error("memory is already allocated");
@@ -20,10 +30,23 @@ template <typename T> class DynamicArray {
             size = 0;
         }
     public: 
+        /**
+         * @brief Конструктор пустого массива
+         * 
+         */
         DynamicArray() : data(nullptr), size(0), capacity(0) {}
+        /**
+         * @brief Конструктор массива с заданной вместимостью
+         * 
+         * @param cap вместимость массива
+         */
         DynamicArray(size_t cap) : data(nullptr), size(0), capacity(0) {
             memory_allocate(cap);
         }
+        /**
+         * @brief Деструктор массива
+         * 
+         */
         ~DynamicArray() {
             if (data != nullptr) {
                 for (size_t i = 0; i < size; i++) {
