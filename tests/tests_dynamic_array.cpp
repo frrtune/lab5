5ab42e2848d85c7261aafa2f8fd2df02407c1ad1
daria <bdrdria@yandex.ru>
2026-04-05 18:17:13 +0500

    fix: fix logic of dynamic array, remove capacity

--- tests/tests_dynamic_array.cpp
+++ tests/tests_dynamic_array.cpp
@@ -10,39 +10,31 @@ template <typename T> class DynamicArray {
     private:
         T* data;
         size_t size;
-        size_t capacity;
-        /**
-         * @brief Выделяет память для динамического массива без инициализации
-         * 
-         * @param cap вместимость массива
-         */
-        void memory_allocate(size_t cap) {
-            if (data != nullptr) {
-                throw std::logic_error("memory is already allocated");
+        DynamicArray(size_t size, T* prev) : size(size) {
+            data = static_cast<T*>(::operator new(size * sizeof(T)));
+            for (size_t i = 0; i < size; i++) {
+                new(data + i) T(prev[i]);
             }
-            if (cap == 0) {
-                size = 0;
-                capacity = 0;
-                return;
-            }
-            data = static_cast<T*>(::operator new(cap * sizeof(T)));
-            capacity = cap;
-            size = 0;
         }
     public: 
         /**
          * @brief Конструктор пустого массива
          * 
          */
-        DynamicArray() : data(nullptr), size(0), capacity(0) {}
+        DynamicArray() : data(nullptr), size(0) {}
         /**
-         * @brief Конструктор массива с заданной вместимостью
+         * @brief Конструктор массива с заданным размером
          * 
-         * @param cap вместимость массива
+         * @param size размер массива
          */
-        DynamicArray(size_t cap) : data(nullptr), size(0), capacity(0) {
-            memory_allocate(cap);
-        }
+        DynamicArray(size_t size) : DynamicArray() {
+            if (size == 0) return;
+            data = static_cast<T*>(::operator new(size * sizeof(T)));
+            for (size_t i = 0; i < size; i++) {
+                new(data + i) T();
+            }
+            this->size = size;  
+        };
         /**
          * @brief Деструктор массива
          * 
