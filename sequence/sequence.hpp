6671dde7a46741b696e7826b6a3eb584b5a54fa9
daria <bdrdria@yandex.ru>
2026-04-06 23:58:34 +0500

    fix: add missing specifier

--- sequence/sequence.hpp
+++ sequence/sequence.hpp
@@ -12,7 +12,7 @@ class Sequence {
         virtual <T>* GetSubsequence(int startIndex, int endIndex) = 0; 
         virtual int GetLength() = 0;
         virtual <T>* Append(T item) = 0; 
-        Sequence<T>*Prepend(T item) = 0; 
+        virtual Sequence<T>*Prepend(T item) = 0; 
         virtual <T>*InsertAt(T item, int index) = 0; 
         virtual Sequence <T>* Concat(Sequence <T> *list) = 0;
 };
