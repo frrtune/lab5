9b4cc08e200233c570521f63763815cf8b36afd9
daria <bdrdria@yandex.ru>
2026-04-06 23:57:25 +0500

    fix: add missing definition

--- sequence/sequence.hpp
+++ sequence/sequence.hpp
@@ -6,12 +6,13 @@ template <typename T>
 class Sequence {
     public:
         virtual ~Sequence() = default;
-        virtual T Getfirst() = 0;
+        virtual T GetFirst() = 0;
         virtual T GetLast() = 0;
         virtual T Get(int index) = 0;
         virtual <T>* GetSubsequence(int startIndex, int endIndex) = 0; 
         virtual int GetLength() = 0;
         virtual <T>* Append(T item) = 0; 
+        Sequence<T>*Prepend(T item) = 0; 
         virtual <T>*InsertAt(T item, int index) = 0; 
         virtual Sequence <T>* Concat(Sequence <T> *list) = 0;
 };
