76b3495089a963b1746eadbbe088e6f4d456f762
daria <bdrdria@yandex.ru>
2026-04-20 13:23:37 +0500

    feat: add exception handling

--- ui/main.cpp
+++ ui/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
 #include "ui/ui.hpp"
+#include "exceptions/exceptions.hpp"
 
 int main() {
+    try {
     open_ui();
+    } catch(RangeError& e){
+        std::cerr << "range error occured: " << e.what() << std::endl;
+    } catch(InvalidArgumentError& e){
+        std::cerr << "invalid argument error occured: " << e.what() << std::endl;
+    } catch(FailedAllocationError& e){
+        std::cerr << "failed allocation error occured: " << e.what() << std::endl;
+    }
     return 0;
 }
