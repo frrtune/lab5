3c59e221e554c50a73023c234a1ba8224aa3dcbd
daria <bdrdria@yandex.ru>
2026-05-08 02:21:32 +0500

    fix: fixed methods using size

--- array_sequence/array_sequence.hpp
+++ array_sequence/array_sequence.hpp
@@ -6,8 +6,8 @@
 template <typename T> class ArraySequence : public Sequence <T> {
     private:
         DynamicArray<T> buff;
-        size_t capacity;
-        ArraySequence(const DynamicArray<T>& arr, size_t param_capacity) : buff(arr), capacity(param_capacity) {}
+        size_t size;
+        ArraySequence(const DynamicArray<T>& arr, size_t param_size) : buff(arr), size(param_size) {}
         size_t calculate_capacity(size_t current_capacity, size_t new_size) {
             if (current_capacity >= new_size) return current_capacity;
             size_t new_capacity = 1;
@@ -28,7 +28,7 @@ template <typename T> class ArraySequence : public Sequence <T> {
         }
 
     public:
-        ArraySequence() : buff(), capacity(0) {}
+        ArraySequence() : buff(), size(0) {}
         ArraySequence(const T* items, size_t count) : buff(count), capacity(count){
             for (size_t i = 0; i < count; i++) {
                buff.Set(i, items[i]);
@@ -71,35 +71,31 @@ template <typename T> class ArraySequence : public Sequence <T> {
             return buff.GetLength();
         };
         ArraySequence<T>* Append(const T& item) const override {
-            size_t index = buff.GetLength();
-            size_t new_size = buff.GetLength() + 1;
-            DynamicArray<T> new_buff = make_buff(buff, capacity, new_size);
-            new_buff.Set(index, item);
-            return new ArraySequence<T>(new_buff, new_buff.GetLength());
+            size_t new_size = size + 1;
+            DynamicArray<T> new_buff = make_buff(buff, buff.GetLength(), new_size);
+            new_buff.Set(size, item);
+            return new ArraySequence<T>(new_buff, new_size);
         };
         ArraySequence<T>* Prepend(const T& item) const override {
-            DynamicArray<T> new_buff;
-            new_buff.Resize(buff.GetLength() + 1);
-            new_buff.Set(0, item);
-            for (size_t i = 1; i < new_buff.GetLength(); i++) {
-                new_buff.Set(i, buff.Get(i - 1));
+            size_t new_size = size + 1;
+            DynamicArray<T> new_buff = make_buff(buff, buff.GetLength(), new_size);
+            for (size_t i = new_size; i > 1; i--) {
+                new_buff[i] = new_buff[i - 1];
             }
-            return new ArraySequence<T>(new_buff);
+            new_buff.Set(0, item);
+            return new ArraySequence<T>(new_buff, new_size);
         };
         ArraySequence<T>* InsertAt(const T& item, size_t index) const override {
             if (index > buff.GetLength()) {
                 throw RangeError("index is out of range");
             }
-            DynamicArray<T> new_buff;
-            new_buff.Resize(buff.GetLength() + 1);
-            for (size_t i = 0; i < index; i++) {
-                new_buff.Set(i, buff.Get(i));
-            }
-            new_buff.Set(index, item);
-            for (size_t i = index + 1; i < new_buff.GetLength(); i++) {
+            size_t new_size = size + 1;
+            DynamicArray<T> new_buff = make_buff(buff, buff.GetLength(), new_size);
+             for (size_t i = size; i > index; i--) {
                 new_buff.Set(i, buff.Get(i - 1));
             }
-            return new ArraySequence<T>(new_buff);
+            new_buff.Set(index, item);
+            return new ArraySequence<T>(new_buff, new_size);
         };
         ArraySequence <T>* Concat(Sequence<T>* list) const override {
             if (list == nullptr) {
