d6c56d710f075b312f866ced979496b91dec7de0
daria <bdrdria@yandex.ru>
2026-05-12 22:43:49 +0500

    feat: add constructors & destructor for deque

--- deque/deque.hpp
+++ deque/deque.hpp
@@ -5,6 +5,9 @@ template <template <typename> class Container, typename T>
 class Deque {
 private:
     Container<T> c;
+public:
+    Deque() = default;
+    ~Deque() = default;
+    Deque(const Deque& other_deque) : c(other_deque.c) {}
+    Deque(const Container<T>& other_container) : c(other_container) {}
 };
-
-
