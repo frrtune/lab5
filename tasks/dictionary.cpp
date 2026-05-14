ae318718ce7a83125c44667dc416bbcaa8c0473f
daria <bdrdria@yandex.ru>
2026-05-14 23:47:09 +0500

    feat: add class name index

--- tasks/dictionary.cpp
+++ tasks/dictionary.cpp
@@ -2,11 +2,11 @@
 #include "../array_sequence/array_sequence.hpp"
 #include <string>
 
-/*class NameIndex {
+class NameIndex {
     private:
         std::string word_;
         ArraySequence<size_t> positions_;
     public:
-        NameIndex() : word_(""), positions_(nullptr) {}
+        NameIndex() : word_(""), positions_() {}
         NameIndex(const std::string& word, ArraySequence<size_t> positions) : word_(word), positions_(positions) {}
-};*/
+};
