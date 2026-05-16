2e60a59b425f87e7b20babe20c45c0fb20ef6cd8
daria <bdrdria@yandex.ru>
2026-05-16 18:46:25 +0500

    feat: add methods for name index class

--- tasks/dictionary.cpp
+++ tasks/dictionary.cpp
@@ -9,6 +9,15 @@ class NameIndex {
     public:
         NameIndex() : word_(""), positions_() {}
         NameIndex(const std::string& word, Deque<ArraySequence, size_t>& positions) : word_(word), positions_(positions) {}
+        std::string get_word() const {
+            return word_;
+        }
+        Deque<ArraySequence, size_t> get_positions() const {
+            return positions_;
+        }
+        void add_positions(size_t position) {
+            positions_.push_back(position);
+        }
 };
 
 class DequeString {
@@ -16,7 +25,7 @@ class DequeString {
         Deque<ArraySequence, NameIndex> name_indexes;
     public:
         DequeString() : name_indexes() {}
-        void add_word(NameIndex& word) {
+        void add_word(const std::string& word, size_t position) {
             name_indexes.push_back(word);
         }
         NameIndex get_word() {
