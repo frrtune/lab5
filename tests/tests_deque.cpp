de179ff60b24c6787b26b711b213413fb315a966
daria <bdrdria@yandex.ru>
2026-05-18 13:18:35 +0500

    feat: add tests

--- tests/tests_deque.cpp
+++ tests/tests_deque.cpp
@@ -2,8 +2,17 @@
 #include "../deque/deque.hpp"
 #include "../array_sequence/array_sequence.hpp"
 
-TEST(DequeTest, PushFrontTest) {
+TEST(DequeTest, PushBackTest) {
     Deque<ArraySequence, int> deque;
     deque.push_back(12);
-    EXPECT_EQ(deque.back(), 12);
+    deque.push_back(102);
+    EXPECT_EQ(deque.back(), 102);
+}
+
+TEST(DequeTest, PopBackTest) {
+    Deque<ArraySequence, int> deque;
+    deque.push_back(102);
+    deque.push_back(12);
+    EXPECT_EQ(deque.pop_back(), 12);
+    EXPECT_EQ(deque.back(), 102);
 }
